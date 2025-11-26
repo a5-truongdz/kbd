585927993055c73c0fd7cec18af2578c2f46dc1e
Tr43212 <trinhtuantruong2k1@gmail.com>
2025-11-26 22:22:49 +0700

    v0.1-beta

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # kbd
+Audio feedback when typing.
+
+# Requirements
+- Python - https://www.python.org
+- python-libevdev - https://pypi.org/project/libevdev/
+
+# Quick install
+```
+$ pip install -r requirements.txt
+```
+
+# Pre-setup
+```
+# usermod -aG input <username>
+```
---
+++ touchpad.py
@@ -0,0 +1,44 @@
+import libevdev
+import glob
+import os
+import random
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+import pygame
+pygame.mixer.init()
+
+LEFT = [libevdev.BTN_LEFT, libevdev.BTN_TOUCH]
+
+leftClick = pygame.mixer.Sound("g502x-wireless/primary_down.wav")
+leftRelease = pygame.mixer.Sound("g502x-wireless/primary_up.wav")
+rightClick = pygame.mixer.Sound("g502x-wireless/secondary_down.wav")
+rightRelease = pygame.mixer.Sound("g502x-wireless/secondary_up.wav")
+middleClick = pygame.mixer.Sound("g502x-wireless/middle.wav")
+
+events = glob.glob("/dev/input/event*")
+global fd
+f = False
+for event in events:
+    fd = libevdev.Device(open(event, "rb"))
+    if fd.has(libevdev.EV_ABS):
+        print(f"Using {event}")
+        f = True
+        break
+if not f:
+    print("No mouse found.")
+    exit(1)
+
+while True:
+    for event in fd.events():
+        if event.code in LEFT:
+            if event.value == 1:
+                leftClick.play()
+            else:
+                leftRelease.play()
+        if event.matches(libevdev.BTN_MIDDLE):
+            middleClick.play()
+        if event.matches(libevdev.BTN_RIGHT):
+            if event.value == 1:
+                rightClick.play()
+            else:
+                rightRelease.play()
